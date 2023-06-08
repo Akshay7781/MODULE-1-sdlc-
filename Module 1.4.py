e0017eb4c095049106bdb749543e6f8fcd980151
Akshay7781 <123167377+Akshay7781@users.noreply.github.com>
2023-06-08 14:57:38 +0530

    Module 1.4.py

---
+++ Module 1.4.py
@@ -0,0 +1,8 @@
+What is python?
+->Python is a powerful, open-source high-level & popular programing language.
+->Used for web development, scientific & mathematical application development etc.
+->Provides excellent library support and has a large developer community.
+->Provides easy integration with web services & GUI-based desktop applications.
+->python Provides easy integration with web services & GUI-based desktop applications.
+->by most of the companies such as YouTube, Instagram, Pinterest etc.
+->Extensively used in Data Science and in for developing Machine Learning projects.
