e71ec1d381317236e103ebc1e8103ed49c6b73d3
Akshay7781 <123167377+Akshay7781@users.noreply.github.com>
2023-06-08 14:51:24 +0530

    Module 1.1.py

---
+++ Module 1.1.py
@@ -0,0 +1,6 @@
+What is software?
+
+-> Software are the instructions given to a computer in the form of a program.
+->Software is the set of programs, which are used for different purposes.
+
+->Software are all the programs used in a computer to perform certain tasks.
