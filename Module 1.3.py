f6b02c1e2988a145d31fb6f1b69a2a86d6f6fb3b
Akshay7781 <123167377+Akshay7781@users.noreply.github.com>
2023-06-08 14:53:12 +0530

    Module 1.3.py

---
+++ Module 1.3.py
@@ -0,0 +1,4 @@
+What is programming?
+
+->Programming is the process of giving computers instructions on how to act.
+ These instructions are known as code, and programmers use code to complete certain tasks.
